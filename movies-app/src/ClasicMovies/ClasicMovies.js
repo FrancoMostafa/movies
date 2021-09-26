a309e9b439b43f218158419795677c9a9ea4cc73
FrancoMostafa <francom2342@gmail.com>
2021-09-26 12:38:48 -0300

    Final V1

--- movies-app/src/ClasicMovies/ClasicMovies.js
+++ movies-app/src/ClasicMovies/ClasicMovies.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "antd/dist/antd.css";
+import MovieList from "../MovieList/MovieList";
 
 export default function ClasicMovies() {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    obtenerDatos();
+  }, []);
+
+  const obtenerDatos = async () => {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/top_rated?api_key=273c9de3a548b17ca4aeca62ccaf85c6&language=EN&page="
+    );
+    const movies = await response.json();
+    setMovieList(movies);
+  };
+
   return (
     <>
-      <p>Proximamente...</p>
+      <MovieList movies={movieList} />
     </>
   );
 }
--- movies-app/src/Home/Home.js
+++ movies-app/src/Home/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
 
   const obtenerDatos = async () => {
     const response = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?api_key=273c9de3a548b17ca4aeca62ccaf85c6&language=es-MX&page="
+      "https://api.themoviedb.org/3/movie/now_playing?api_key=273c9de3a548b17ca4aeca62ccaf85c6&language=EN&page="
     );
     const movies = await response.json();
     setMovieList(movies);
